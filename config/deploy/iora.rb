e98b395c348d5fae2b6a472ba23073eedc6d5393
Alex Rothenberg <alex@alexrothenberg.com>
2016-04-08 11:53:58 -0400

    iora deploy

--- config/deploy/iora.rb
+++ config/deploy/iora.rb
@@ -4,6 +4,8 @@ require "./config/deploy_hosts"
 # hosts have the primary property set.
 role :worker, HostSettings.worker_hosts
 
+server 'kochiku.icisapp.com', user: 'ops', roles: %w{web app db worker}
+
 # Set deploy_to to the path where you would like kochiku-worker to be deployed
 # to on the server.
 set :deploy_to, '/home/ops/kochiku-worker'
